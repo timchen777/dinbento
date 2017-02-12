36ad238d8a7aff9a1f96b4374408dffa766f9ea5
timchen7@yahoo.com <timchen7@yahoo.com>
2017-02-12 00:11:36 -0800

    v2

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   resources :orders
 
   root 'orders#index'
+
+  resources :users, only: [:new, :create]
+  get '/register', to: 'users#new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
